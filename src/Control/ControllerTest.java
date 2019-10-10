604ec558dab0140e56d3e89b5abc1dd986d0f452
zhen.wang3 <zhen.wang3@ucalgary.ca>
2019-10-09 21:47:20 -0600

    Test Case

--- src/Control/ControllerTest.java
+++ src/Control/ControllerTest.java
@@ -9,7 +9,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ControllerTest {
-
+	int maxState;
+	int minState;
+	boolean activity;
+	int currentState;
+	int ambientState;
+	int ambientRate;
+	int desiredState;
+	int desiredRate;
+	int upperBound;
+	int lowerBound;
+	int refresh;
+	int higherValue;
+	int lowerValue;
 
 	
 	@BeforeAll
@@ -21,7 +33,19 @@ class ControllerTest {
 	}
 
 	@BeforeEach
-	void setUp() throws Exception {
+	void setUp() throws Exception {	
+	activity = true;
+	currentState = 50;
+	ambientState = 35;
+	ambientRate = 23;
+	desiredState = 80;
+	desiredRate = 17;
+	upperBound = 4;
+	lowerBound = 2;
+	refresh = 1;
+	higherValue = 90;
+	lowerValue = 10;
+
 	}
 
 	@AfterEach
@@ -29,14 +53,129 @@ class ControllerTest {
 	}
 
 	@Test
-	void test() {
+	void testController() {
 		Controller temperatureController = new Controller();
-		assertEquals(1000000, temperatureController.getMaxState());
-		assertEquals(-271, temperatureController.getMinState());
 		
-		OneWayController humidityController = new OneWayController();
-		assertEquals(100, humidityController.getMaxState());
-		assertEquals(0, humidityController.getMinState());
+		maxState = 1000000;
+		minState = -271;
+		
+		assertEquals(maxState, temperatureController.getMaxState());
+		
+		assertEquals(minState, temperatureController.getMinState());
+		
+		temperatureController.setActivity(activity);
+		assertEquals(activity, temperatureController.getActivity());
+		
+		temperatureController.setCurrentState(currentState);
+		assertEquals(currentState, temperatureController.getCurrentState());
+		
+		temperatureController.setAmbientState(ambientState);
+		assertEquals(ambientState, temperatureController.getAmbientState());
+		
+		temperatureController.setAmbientRate(ambientRate);
+		assertEquals(ambientRate, temperatureController.getAmbientRate());
+		
+		temperatureController.setDesiredState(desiredState);
+		assertEquals(desiredState, temperatureController.getDesiredState());
+		
+		temperatureController.setDesiredRate(desiredRate);
+		assertEquals(desiredRate, temperatureController.getDesiredRate());
+		
+		temperatureController.setDesiredUpperBound(upperBound);
+		assertEquals(upperBound, temperatureController.getDesiredUpperBound());
+		
+		temperatureController.setDesiredLowerBound(lowerBound);
+		assertEquals(lowerBound, temperatureController.getDesiredLowerBound());
+		
+		temperatureController.setRefresh(refresh);
+		assertEquals(refresh, temperatureController.getRefresh());
+		
+		int negativeAR = 0-Math.abs(ambientRate);
+		temperatureController.setCurrentState(higherValue);
+		temperatureController.setAmbientState(lowerValue);
+		temperatureController.updateAmbientRate();
+		assertEquals(negativeAR, temperatureController.getAmbientRate());
+		
+		int positiveAR = Math.abs(ambientRate);
+		temperatureController.setCurrentState(lowerValue);
+		temperatureController.setAmbientState(higherValue);
+		temperatureController.updateAmbientRate();
+		assertEquals(positiveAR, temperatureController.getAmbientRate());
+		
+		int negativeDR = 0-Math.abs(desiredRate);
+		temperatureController.setCurrentState(higherValue);
+		temperatureController.setDesiredState(lowerValue);
+		temperatureController.updateDesiredRate();
+		assertEquals(negativeDR, temperatureController.getDesiredRate());
+		
+		int positiveDR = Math.abs(desiredRate);
+		temperatureController.setCurrentState(lowerValue);
+		temperatureController.setDesiredState(higherValue);
+		temperatureController.updateDesiredRate();
+		assertEquals(positiveDR, temperatureController.getDesiredRate());
+	}
+	
+	@Test
+	void testOneWayController() {
+		OneWayController oneWayController = new OneWayController();
+		
+		maxState = 100;
+		minState = 0;
+		
+		assertEquals(maxState, oneWayController.getMaxState());
+		
+		assertEquals(minState, oneWayController.getMinState());
+		
+		oneWayController.setActivity(activity);
+		assertEquals(activity, oneWayController.getActivity());
+		
+		oneWayController.setCurrentState(currentState);
+		assertEquals(currentState, oneWayController.getCurrentState());
+		
+		oneWayController.setAmbientState(ambientState);
+		assertEquals(ambientState, oneWayController.getAmbientState());
+		
+		oneWayController.setAmbientRate(ambientRate);
+		assertEquals(ambientRate, oneWayController.getAmbientRate());
+		
+		oneWayController.setDesiredState(desiredState);
+		assertEquals(desiredState, oneWayController.getDesiredState());
+		
+		oneWayController.setDesiredRate(desiredRate);
+		assertEquals(desiredRate, oneWayController.getDesiredRate());
+		
+		oneWayController.setDesiredUpperBound(upperBound);
+		assertEquals(upperBound, oneWayController.getDesiredUpperBound());
+		
+		oneWayController.setDesiredLowerBound(lowerBound);
+		assertEquals(lowerBound, oneWayController.getDesiredLowerBound());
+		
+		oneWayController.setRefresh(refresh);
+		assertEquals(refresh, oneWayController.getRefresh());
+		
+		int negativeAR = 0-Math.abs(ambientRate);
+		oneWayController.setCurrentState(higherValue);
+		oneWayController.setAmbientState(lowerValue);
+		oneWayController.updateAmbientRate();
+		assertEquals(negativeAR, oneWayController.getAmbientRate());
+		
+		int positiveAR = Math.abs(ambientRate);
+		oneWayController.setCurrentState(lowerValue);
+		oneWayController.setAmbientState(higherValue);
+		oneWayController.updateAmbientRate();
+		assertEquals(positiveAR, oneWayController.getAmbientRate());
+		
+		int negativeDR = 0-Math.abs(desiredRate);
+		oneWayController.setCurrentState(higherValue);
+		oneWayController.setDesiredState(lowerValue);
+		oneWayController.updateDesiredRate();
+		assertEquals(negativeDR, oneWayController.getDesiredRate());
+		
+		int positiveDR = Math.abs(desiredRate);
+		oneWayController.setCurrentState(lowerValue);
+		oneWayController.setDesiredState(higherValue);
+		oneWayController.updateDesiredRate();
+		assertEquals(positiveDR, oneWayController.getDesiredRate());
 	}
 
 }
